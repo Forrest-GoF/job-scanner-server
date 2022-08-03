5634a74bf55470ee75d1aa8d4a6673c214dc06cb
kang <rilac1@naver.com>
2022-08-03 18:35:51 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/forrestgof/jobscanner/JobscannerApplication.java
@@ -0,0 +1,13 @@
+package com.forrestgof.jobscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobscannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobscannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forrestgof/jobscanner/JobscannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forrestgof.jobscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobscannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
