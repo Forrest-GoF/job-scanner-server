762f70affdfb6cf6c0e1a66ca6f7d01974bb76b2
kang <rilac1@naver.com>
2022-08-05 23:31:29 +0900

    :sparkles: [GOF-106] 공고 관련 도메인 작성

---
+++ src/main/java/com/forrestgof/jobscanner/jobposting/domain/JobPosting.java
@@ -0,0 +1,58 @@
+package com.forrestgof.jobscanner.jobposting.domain;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "JOB")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class JobPosting {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "job_id")
+	private Long id;
+
+	private String googleKey;
+
+	@Column(name = "job_title")
+	private String title;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "company_id")
+	private Company company;
+
+	@Column(name = "job_location")
+	private String location;
+
+	private LocalDateTime postedAt;
+	private LocalDateTime expiredAt;
+	private String platform;
+	private String applyUrl;
+	private String type;
+	private int salary;
+	private String education;
+	private String career;
+	private JobDetail jobDetail;
+	private List<JobStack> jobStacks = new ArrayList<>();
+
+}
