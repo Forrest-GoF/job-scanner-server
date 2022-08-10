56709f00817f247be0808f5e6b7145b2087fa5df
Juwon Park <devprofessionalism@gmail.com>
2022-08-11 02:44:57 +0900

    :sparkles: ApiResponse 정의

---
+++ src/main/java/com/forrestgof/jobscanner/common/dto/ApiResponse.java
@@ -0,0 +1,46 @@
+package com.forrestgof.jobscanner.common.dto;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.forrestgof.jobscanner.common.exception.ErrorCode;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+	private boolean success;
+	private T data;
+	private ErrorResponse error;
+
+	static <T> ResponseEntity toResponseEntity(T data) {
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(ApiResponse.builder()
+				.success(true)
+				.data(data)
+				.build());
+	}
+
+	static <T> ResponseEntity toResponseEntity(HttpStatus httpStatus, T data) {
+		return ResponseEntity.status(httpStatus)
+			.body(ApiResponse.builder()
+				.success(true)
+				.data(data)
+				.build());
+	}
+
+	static ResponseEntity toResponseEntity(ErrorCode e) {
+		return ResponseEntity.status(e.getStatus())
+			.body(ApiResponse.builder()
+				.success(false)
+				.data(null)
+				.error(ErrorResponse.of(e))
+				.build());
+	}
+
+}
