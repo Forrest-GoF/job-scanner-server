0be4f2f30398fa774b820031a97a26aa13961ad4
kang <rilac1@naver.com>
2022-08-12 20:49:49 +0900

    [GOF-106] :sparkles: 크롤링 배치 테스트 작성

---
+++ src/test/java/com/forrestgof/jobscanner/jobposting/util/crawler/BatchUtilsTest.java
@@ -0,0 +1,41 @@
+package com.forrestgof.jobscanner.jobposting.util.crawler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.forrestgof.jobscanner.jobposting.domain.JobPosting;
+import com.forrestgof.jobscanner.jobposting.service.JobPostingService;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class BatchUtilsTest {
+
+	@Autowired
+	BatchUtils batchUtils;
+	@Autowired JobPostingService jobPostingService;
+
+	@Test
+	public void batchCrawlingTest() throws Exception {
+	    //given
+		List<String> searchKeywords = new ArrayList<>();
+		searchKeywords.add("스프링백엔드개발자");
+
+		//when
+		batchUtils.batchCrawling(searchKeywords);
+
+	    //then
+		List<JobPosting> findAll = jobPostingService.findAll();
+		for (JobPosting jobPosting : findAll) {
+			System.out.println("jobPosting = " + jobPosting.getTitle());
+		}
+
+		Assertions.assertThat(findAll.size()).isGreaterThan(0);
+	}
+}
