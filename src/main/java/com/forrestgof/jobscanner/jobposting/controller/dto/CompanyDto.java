ec3dae546ba8ebe0fa0550bcf54550d3a7c7cdb4
kang <rilac1@naver.com>
2022-08-12 22:17:54 +0900

    [GOF-108] :sparkles: Job API 반환 DTO 정의

--- src/main/java/com/forrestgof/jobscanner/jobposting/controller/dto/CompanyDto.java
+++ src/main/java/com/forrestgof/jobscanner/jobposting/controller/dto/CompanyDto.java
@@ -1,8 +1,6 @@
 package com.forrestgof.jobscanner.jobposting.controller.dto;
 
-import java.time.format.DateTimeFormatter;
-
-import com.forrestgof.jobscanner.jobposting.domain.Company;
+import com.forrestgof.jobscanner.company.domain.Company;
 
 import lombok.Data;
 
@@ -19,8 +17,9 @@ public class CompanyDto {
 		name = company.getName();
 		employees = company.getEmployees();
 		averageSalary = company.getAverageSalary();
-		registrationDate = company.getRegistrationDate().format(DateTimeFormatter.ISO_LOCAL_DATE);
+		if (company.getRegistrationDate() != null)
+			registrationDate = company.getRegistrationDate().toString();
 		thumbnailUrl = company.getThumbnailUrl();
-		companyLocation = company.getCompanyLocation();
+		companyLocation = company.getAddress();
 	}
 }
