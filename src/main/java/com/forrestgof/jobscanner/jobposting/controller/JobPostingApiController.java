9c4dd2309338e343a01d70c94fd2130f87b265ca
kang <rilac1@naver.com>
2022-08-13 19:37:47 +0900

    :adhesive_bandage: API 반환 스펙 재정의

--- src/main/java/com/forrestgof/jobscanner/jobposting/controller/JobPostingApiController.java
+++ src/main/java/com/forrestgof/jobscanner/jobposting/controller/JobPostingApiController.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.forrestgof.jobscanner.common.util.CustomResponse;
 import com.forrestgof.jobscanner.jobposting.controller.dto.JobDto;
 import com.forrestgof.jobscanner.jobposting.controller.dto.JobPreviewDto;
 import com.forrestgof.jobscanner.jobposting.domain.JobPosting;
@@ -27,7 +29,7 @@ public class JobPostingApiController {
 
 	@GetMapping("/api/jobs")
 	@ResponseBody
-	public Result getFilterJobs(
+	public ResponseEntity<Result> getFilterJobs(
 		@RequestParam(required = false) Map<String, String> params
 	) {
 		List<JobPosting> all = jobPostingService.findFilterJobs(params);
@@ -35,16 +37,16 @@ public class JobPostingApiController {
 			.map(JobPreviewDto::new)
 			.collect(Collectors.toList());
 
-		return new Result(previewDtos, previewDtos.size());
+		return CustomResponse.success(new Result(previewDtos, previewDtos.size()));
 	}
 
 	@GetMapping("api/jobs/{id}")
 	@ResponseBody
-	public JobDto getJob(
+	public ResponseEntity<JobDto> getJob(
 		@PathVariable Long id
 	) {
 		JobPosting findOne = jobPostingService.findOne(id);
-		return new JobDto(findOne);
+		return CustomResponse.success(new JobDto(findOne));
 	}
 
 	@Data
