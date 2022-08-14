9ead5dd7c9a63a45235cf4f385e2ef1b912634b1
Juwon Park <devprofessionalism@gmail.com>
2022-08-15 06:10:00 +0900

    [GOF-131] :sparkles: KakaoUserResponse 정의, KakaoAuthService 작성

---
+++ src/main/java/com/forrestgof/jobscanner/auth/service/KakaoAuthService.java
@@ -0,0 +1,68 @@
+package com.forrestgof.jobscanner.auth.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.forrestgof.jobscanner.auth.dto.AuthLoginResponse;
+import com.forrestgof.jobscanner.auth.dto.KakaoUserResponse;
+import com.forrestgof.jobscanner.auth.jwt.AuthTokenProvider;
+import com.forrestgof.jobscanner.common.exception.CustomException;
+import com.forrestgof.jobscanner.common.exception.ErrorCode;
+import com.forrestgof.jobscanner.member.domain.Member;
+import com.forrestgof.jobscanner.member.service.MemberService;
+import com.forrestgof.jobscanner.session.service.SessionService;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Service
+public class KakaoAuthService extends AuthService {
+
+	private KakaoAuthService(
+		WebClient webClient,
+		AuthTokenProvider authTokenProvider,
+		MemberService memberService,
+		SessionService sessionService) {
+		super(webClient, authTokenProvider, memberService, sessionService);
+	}
+
+	@Override
+	protected Member getMemberFromAccessToken(String accessToken) {
+		KakaoUserResponse kakaoUserResponse = webClient.get()
+			.uri("https://kapi.kakao.com/v2/user/me")
+			.headers(h -> h.setBearerAuth(accessToken))
+			.retrieve()
+			.onStatus(HttpStatus::is4xxClientError, response
+				-> Mono.error(
+				new CustomException("Social Access Token is unauthorized", ErrorCode.INVALID_TOKEN_EXCEPTION)))
+			.onStatus(HttpStatus::is5xxServerError, response
+				-> Mono.error(new CustomException("Internal Server Error", ErrorCode.INVALID_TOKEN_EXCEPTION)))
+			.bodyToMono(KakaoUserResponse.class)
+			.block();
+
+		String email = kakaoUserResponse.getKakaoAccount().getEmail();
+		String nickname = kakaoUserResponse.getProperties().getNickname();
+		String imageUrl = kakaoUserResponse.getProperties().getProfileImage();
+		if (email == null || nickname == null) {
+			throw new CustomException(ErrorCode.INVALID_TOKEN_EXCEPTION);
+		}
+
+		return Member.builder()
+			.email(email)
+			.nickname(nickname)
+			.imageUrl(imageUrl)
+			.build();
+	}
+
+	public void signup(String accessToken) {
+		Member member = getMemberFromAccessToken(accessToken);
+		super.signupByMember(member);
+	}
+
+	public AuthLoginResponse login(String accessToken) {
+		Member member = getMemberFromAccessToken(accessToken);
+		return super.loginByEmail(member.getEmail());
+	}
+}
