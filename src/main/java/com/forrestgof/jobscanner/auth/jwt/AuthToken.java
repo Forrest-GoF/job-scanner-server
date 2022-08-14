374806d9c6024f462851ffc8a90069c429a47e73
Juwon Park <devprofessionalism@gmail.com>
2022-08-15 06:02:52 +0900

    [GOF-131] :sparkles: JWT 발급 로직 구현

---
+++ src/main/java/com/forrestgof/jobscanner/auth/jwt/AuthToken.java
@@ -0,0 +1,86 @@
+package com.forrestgof.jobscanner.auth.jwt;
+
+import java.security.Key;
+import java.util.Date;
+
+import com.forrestgof.jobscanner.auth.RoleType;
+import com.forrestgof.jobscanner.common.exception.CustomException;
+import com.forrestgof.jobscanner.common.exception.ErrorCode;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@RequiredArgsConstructor
+public class AuthToken {
+
+	private final String appToken;
+	private final String refreshToken;
+	private final Key appKey;
+	private final Key refreshKey;
+
+	private static final String AUTHORITIES_KEY = "role";
+
+	public AuthToken(String appTokenUuid, String refreshTokenUuid,
+		RoleType roleType,
+		Date appTokenExpiry, Date refreshTokenExpiry,
+		Key appKey, Key refreshKey) {
+
+		String role = roleType.toString();
+
+		this.appKey = appKey;
+		this.refreshKey = refreshKey;
+		this.appToken = createToken(appTokenUuid, role, this.appKey, appTokenExpiry);
+		this.refreshToken = createToken(refreshTokenUuid, role, this.refreshKey, refreshTokenExpiry);
+	}
+
+	private String createToken(String uuid, String role, Key key, Date expiry) {
+		return Jwts.builder()
+			.claim("jti", uuid)
+			.claim(AUTHORITIES_KEY, role)
+			.signWith(key, SignatureAlgorithm.HS256)
+			.setExpiration(expiry)
+			.compact();
+	}
+
+	public Claims getAppTokenClaims() {
+		return getTokenClaims(this.appToken, this.appKey);
+	}
+
+	public Claims getAppTokenClaimsIncludingExpired() {
+		try {
+			return Jwts.parserBuilder()
+				.setSigningKey(this.appKey)
+				.build()
+				.parseClaimsJws(this.appToken)
+				.getBody();
+		} catch (ExpiredJwtException e) {
+			return e.getClaims();
+		} catch (SecurityException | JwtException | IllegalArgumentException e) {
+			throw new CustomException(ErrorCode.INVALID_TOKEN_EXCEPTION);
+		}
+	}
+
+	public Claims getRefreshTokenClaims() {
+		return getTokenClaims(this.refreshToken, this.refreshKey);
+	}
+
+	private Claims getTokenClaims(String token, Key key) {
+		try {
+			return Jwts.parserBuilder()
+				.setSigningKey(key)
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+		} catch (SecurityException | JwtException | IllegalArgumentException e) {
+			throw new CustomException(ErrorCode.INVALID_TOKEN_EXCEPTION);
+		}
+	}
+}
---
+++ src/main/java/com/forrestgof/jobscanner/auth/jwt/AuthTokenProvider.java
@@ -0,0 +1,75 @@
+package com.forrestgof.jobscanner.auth.jwt;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import com.forrestgof.jobscanner.auth.RoleType;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class AuthTokenProvider {
+
+	@Value("${app.auth.app-token-expiry}")
+	private String appTokenExpiry;
+	@Value("${app.auth.refresh-token-expiry}")
+	private String refreshTokenExpiry;
+
+	private final Key appKey;
+	private final Key refreshKey;
+	private static final String AUTHORITIES_KEY = "role";
+
+	public AuthTokenProvider(
+		@Value("${app.auth.app-token-secret}") String appSecretKey,
+		@Value("${app.auth.refresh-token-secret}") String refreshSecretKey) {
+
+		this.appKey = Keys.hmacShaKeyFor(appSecretKey.getBytes());
+		this.refreshKey = Keys.hmacShaKeyFor(refreshSecretKey.getBytes());
+	}
+
+	public AuthToken createToken(String appTokenUuid, String refreshTokenUuid, RoleType roleType,
+		String appTokenExpiry, String refreshTokenExpiry) {
+
+		Date appTokenExpiryDate = new Date(System.currentTimeMillis() + Long.parseLong(appTokenExpiry));
+		Date refreshTokenExpiryDate = new Date(System.currentTimeMillis() + Long.parseLong(refreshTokenExpiry));
+		return new AuthToken(appTokenUuid, refreshTokenUuid, roleType, appTokenExpiryDate, refreshTokenExpiryDate,
+			this.appKey, this.refreshKey);
+	}
+
+	public AuthToken createUserAuthToken(String appTokenUuid, String refreshTokenUuid) {
+		return createToken(appTokenUuid, refreshTokenUuid, RoleType.USER, this.appTokenExpiry, this.refreshTokenExpiry);
+	}
+
+	public AuthToken convertAuthToken(String appToken, String refreshToken) {
+		return new AuthToken(appToken, refreshToken, this.appKey, this.refreshKey);
+	}
+
+	public AuthToken convertAuthToken(String appToken) {
+		return new AuthToken(appToken, appToken, this.appKey, this.refreshKey);
+	}
+
+	public Authentication getAuthentication(AuthToken authToken) {
+
+		Claims claims = authToken.getAppTokenClaims();
+		Collection<? extends GrantedAuthority> authorities =
+			Arrays.stream(new String[] {claims.get(AUTHORITIES_KEY).toString()})
+				.map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toList());
+
+		User principal = new User(claims.get("jti", String.class), "", authorities);
+
+		return new UsernamePasswordAuthenticationToken(principal, authToken, authorities);
+	}
+}
