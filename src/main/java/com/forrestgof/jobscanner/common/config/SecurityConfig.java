693dc046bcc752df338e5cd686866da00a515d58
kang <rilac1@naver.com>
2022-08-15 21:04:51 +0900

    :adhesive_bandage: API 경로 수정 Security 반영

--- src/main/java/com/forrestgof/jobscanner/common/config/SecurityConfig.java
+++ src/main/java/com/forrestgof/jobscanner/common/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/", "/api", "/api/jobs/**", "/api/auth/**");
+		web.ignoring().antMatchers("/", "/jobs/**", "/auth/**");
 	}
 
 	@Override
