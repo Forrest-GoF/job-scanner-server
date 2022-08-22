7b0d1227340a16ded56990c1be373dc31d1d1cc2
kang <rilac1@naver.com>
2022-08-23 06:33:57 +0900

    :sparkles: `NotFoundException` 추가

---
+++ src/main/java/com/forrestgof/jobscanner/common/exception/NotFoundException.java
@@ -0,0 +1,13 @@
+package com.forrestgof.jobscanner.common.exception;
+
+public class NotFoundException extends RuntimeException {
+
+	public NotFoundException() {
+		super();
+	}
+
+	public NotFoundException(String message) {
+		super(message);
+	}
+
+}
