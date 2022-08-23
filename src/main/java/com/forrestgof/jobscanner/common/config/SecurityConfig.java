2339d7ad6138f1a81ba3e8d35155bba69c2d45c3
kang <rilac1@naver.com>
2022-08-23 09:10:17 +0900

    [GOF-142] :adhesive_bandage: tags CORS 추가

--- src/main/java/com/forrestgof/jobscanner/common/config/SecurityConfig.java
+++ src/main/java/com/forrestgof/jobscanner/common/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/", "/jobs/**", "/auth/**");
+		web.ignoring().antMatchers("/", "/jobs/**", "/auth/**", "/tags/**");
 	}
 
 	@Override
