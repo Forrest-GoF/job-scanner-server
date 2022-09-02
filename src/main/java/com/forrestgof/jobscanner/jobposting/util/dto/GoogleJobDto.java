0c0a1b7839dd115474549408f8249134b26613c1
kang <rilac1@naver.com>
2022-09-02 16:18:49 +0900

    :recycle: 레코드 타입으로 변환

--- src/main/java/com/forrestgof/jobscanner/jobposting/util/dto/GoogleJobDto.java
+++ src/main/java/com/forrestgof/jobscanner/jobposting/util/dto/GoogleJobDto.java
@@ -1,20 +1,17 @@
 package com.forrestgof.jobscanner.jobposting.util.dto;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+public record GoogleJobDto (
+	String applyUrl,
+	String companyName,
+	String description,
+	String key,
+	String location,
+	String platform,
+	String postedAt,
+	String salary,
+	String thumbnail,
+	String title,
+	String type
+) {
 
-@Data
-@NoArgsConstructor
-public class GoogleJobDto {
-	String applyUrl;
-	String companyName;
-	String description;
-	String key;
-	String location;
-	String platform;
-	String postedAt;
-	String salary;
-	String thumbnail;
-	String title;
-	String type;
 }
