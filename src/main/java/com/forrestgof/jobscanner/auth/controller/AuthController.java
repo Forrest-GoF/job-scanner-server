021986dae33d26e2849ff1587bb2cb601b29b436
Juwon Park <devprofessionalism@gmail.com>
2022-09-07 14:48:32 +0900

    :sparkles: 처음 social signin할 때 회원가입 로직 작성

--- src/main/java/com/forrestgof/jobscanner/auth/controller/AuthController.java
+++ src/main/java/com/forrestgof/jobscanner/auth/controller/AuthController.java
@@ -1,51 +1,77 @@
 package com.forrestgof.jobscanner.auth.controller;
 
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.forrestgof.jobscanner.auth.dto.AuthLoginResponse;
 import com.forrestgof.jobscanner.auth.dto.AuthRefreshResponse;
 import com.forrestgof.jobscanner.auth.jwt.JwtHeaderUtil;
 import com.forrestgof.jobscanner.auth.service.AuthService;
 import com.forrestgof.jobscanner.auth.social.SocialTokenValidator;
+import com.forrestgof.jobscanner.auth.social.SocialTokenValidatorFactory;
 import com.forrestgof.jobscanner.common.util.CustomResponse;
 import com.forrestgof.jobscanner.member.domain.Member;
 import com.forrestgof.jobscanner.member.service.MemberService;
+import com.forrestgof.jobscanner.socialmember.domain.SocialMember;
+import com.forrestgof.jobscanner.socialmember.service.SocialMemberService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestController
-@RequestMapping("/auth")
+@Controller
+@RequestMapping("auth")
 @RequiredArgsConstructor
 public class AuthController {
 
 	private final MemberService memberService;
-	private final SocialTokenValidator socialTokenValidator;
+	private final SocialMemberService socialMemberService;
 	private final AuthService authService;
+	private final SocialTokenValidatorFactory socialTokenValidatorFactory;
+	private SocialTokenValidator socialTokenValidator;
 
-	@PostMapping("/signup/kakao")
-	public ResponseEntity signup(HttpServletRequest request) {
-		String accessToken = JwtHeaderUtil.getAccessToken(request);
-		Member member = socialTokenValidator.generateMemberFromAccessToken(accessToken);
+	@PostMapping("signin/{socialType}")
+	public ResponseEntity<AuthLoginResponse> socialSignin(
+		@PathVariable("socialType") String socialType,
+		HttpServletRequest request) {
+		HttpStatus httpStatus = HttpStatus.OK;
+		socialTokenValidator = socialTokenValidatorFactory.find(socialType);
+		String code = JwtHeaderUtil.getCode(request);
+		Member member = socialTokenValidator.generateMemberFromCode(code);
+		Optional<SocialMember> findSocialMember = socialMemberService.findByEmailAndType(member.getEmail(), socialType);
+		if (findSocialMember.isEmpty()) {
+			findSocialMember = Optional.ofNullable(socialSignup(member, socialType));
+			httpStatus = HttpStatus.CREATED;
+		}
+		return ResponseEntity.status(httpStatus).body(authService.signin(findSocialMember.get()));
+	}
+
+	private SocialMember socialSignup(Member member, String socialTYpe) {
+		Member findMember = memberService.findByEmail(member.getEmail())
+			.orElseGet(() -> signup(member));
+		SocialMember socialMember = SocialMember.builder()
+			.member(findMember)
+			.email(member.getEmail())
+			.type(socialTYpe)
+			.build();
+		socialMemberService.save(socialMember);
+		return socialMemberService.findByEmailAndType(socialMember.getEmail(), socialMember.getType()).get();
+	}
+
+	private Member signup(Member member) {
 		memberService.save(member);
-		return CustomResponse.success();
+		return memberService.findByEmail(member.getEmail()).get();
 	}
 
-	@PostMapping("/signin/kakao")
-	public ResponseEntity<AuthLoginResponse> kakaoAuthRequest(HttpServletRequest request) {
-		String accessToken = JwtHeaderUtil.getAccessToken(request);
-		Member member = socialTokenValidator.generateMemberFromAccessToken(accessToken);
-		Member findMember = memberService.findByEmail(member.getEmail());
-		return CustomResponse.success(authService.signin(findMember));
-	}
-
-	@PostMapping("/refresh")
+	@PostMapping("refresh")
 	public ResponseEntity<AuthRefreshResponse> refreshToken(HttpServletRequest request) {
 		String appToken = JwtHeaderUtil.getAccessToken(request);
 		String refreshToken = JwtHeaderUtil.getRefreshToken(request);
