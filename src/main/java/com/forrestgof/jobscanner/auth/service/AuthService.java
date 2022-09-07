85e85a3d53fef239b0541ae9e6ef1ece0a11eeae
Juwon Park <devprofessionalism@gmail.com>
2022-09-07 14:38:08 +0900

    :sparkles: Member에 password 컬럼 추가

--- src/main/java/com/forrestgof/jobscanner/auth/service/AuthService.java
+++ src/main/java/com/forrestgof/jobscanner/auth/service/AuthService.java
@@ -3,10 +3,13 @@ package com.forrestgof.jobscanner.auth.service;
 import com.forrestgof.jobscanner.auth.dto.AuthLoginResponse;
 import com.forrestgof.jobscanner.auth.dto.AuthRefreshResponse;
 import com.forrestgof.jobscanner.member.domain.Member;
+import com.forrestgof.jobscanner.socialmember.domain.SocialMember;
 
 public interface AuthService {
 
 	AuthLoginResponse signin(Member findMember);
+
+	AuthLoginResponse signin(SocialMember socialMember);
 
 	AuthRefreshResponse refreshToken(String appToken, String refreshToken);
 }
