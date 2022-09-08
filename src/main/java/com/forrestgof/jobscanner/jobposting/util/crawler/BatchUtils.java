2bed3fc2999bbebbc95362bf72364fc97ea3e4fa
kang <rilac1@naver.com>
2022-09-09 01:23:28 +0900

    [GOF-178] :recycle: BatchUtils 리펙토링

--- src/main/java/com/forrestgof/jobscanner/jobposting/util/crawler/BatchUtils.java
+++ src/main/java/com/forrestgof/jobscanner/jobposting/util/crawler/BatchUtils.java
@@ -1,14 +1,10 @@
 package com.forrestgof.jobscanner.jobposting.util.crawler;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
 import org.springframework.stereotype.Component;
 
-import com.forrestgof.jobscanner.jobposting.service.JobPostingService;
 import com.forrestgof.jobscanner.jobposting.util.dto.GoogleCrawlingResponse;
-import com.forrestgof.jobscanner.jobposting.util.dto.GoogleJobDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,28 +14,18 @@ public class BatchUtils {
 
 	private final GoogleJobCrawler googleJobCrawler;
 	private final CrawlingDataParser crawlingDataParser;
-	private final JobPostingService jobPostingService;
 
 	public void batchCrawling(List<String> searchKeywords) {
-		for (String searchKeyword : searchKeywords) {
-			int offset = 0;
-			while(true) {
-				GoogleCrawlingResponse googleCrawlingResponse = googleJobCrawler.callCrawler(searchKeyword, offset);
-				if (googleCrawlingResponse.getLength() == 0)
-					break;
-
-				filterNewJob(googleCrawlingResponse)
-					.forEach(crawlingDataParser::saveGoogleJob);
-				offset += 10;
-			}
-		}
+		searchKeywords.forEach(this::search);
 	}
 
-	private List<GoogleJobDto> filterNewJob(GoogleCrawlingResponse googleCrawlingResponse) {
-		List<GoogleJobDto> newJobs = new ArrayList<>();
-		googleCrawlingResponse.getData().stream()
-			.filter(Predicate.not(job -> jobPostingService.existsByGoogleKey(job.key())))
-			.forEach(newJobs::add);
-		return newJobs;
+	private void search(String searchKeyword) {
+		for (int offset=0; true; offset+=10) {
+			GoogleCrawlingResponse googleCrawlingResponse = googleJobCrawler.callCrawler(searchKeyword, offset);
+			if (googleCrawlingResponse.getLength() == 0) {
+				break;
+			}
+			googleCrawlingResponse.getData().forEach(crawlingDataParser::parse);
+		}
 	}
 }
