ac30f856cf6d130775ab2d934fcda3f681e1cc8b
kang <rilac1@naver.com>
2022-09-09 04:22:27 +0900

    [GOF-178] :loud_sound: 예외 발생시 전역 핸들러에서 로그 생성

--- src/main/java/com/forrestgof/jobscanner/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/forrestgof/jobscanner/common/exception/GlobalExceptionHandler.java
@@ -6,21 +6,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.forrestgof.jobscanner.common.util.CustomResponse;
 
+import lombok.extern.log4j.Log4j2;
+
 @RestControllerAdvice
+@Log4j2
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(CustomException.class)
 	public ResponseEntity customExceptionHandle(CustomException e) {
+		log.error(e);
 		return CustomResponse.error(e.getErrorCode());
 	}
 
 	@ExceptionHandler(UndefinedException.class)
 	public ResponseEntity undefinedExceptionHandle(UndefinedException e) {
+		log.error(e);
 		return CustomResponse.error(e.getMessage());
 	}
 
 	@ExceptionHandler
 	public ResponseEntity unexpectedExceptionHandle(Exception e) {
+		log.error(e);
 		return CustomResponse.error(ErrorCode.UNEXPECTED_EXCEPTION);
 	}
 }
