33bc742088f27c0bf1ea1f32f1aa1eeaf10890dc
kang <rilac1@naver.com>
2022-09-13 00:55:24 +0900

    [GOF-174] :sparkles: 직무 조회 로직 작성

---
+++ src/main/java/com/forrestgof/jobscanner/duty/repository/MemberDutyRepository.java
@@ -0,0 +1,13 @@
+package com.forrestgof.jobscanner.duty.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.forrestgof.jobscanner.duty.domain.MemberDuty;
+import com.forrestgof.jobscanner.member.domain.Member;
+
+public interface MemberDutyRepository extends JpaRepository<MemberDuty, Long> {
+
+	List<MemberDuty> findByMember(Member member);
+}
