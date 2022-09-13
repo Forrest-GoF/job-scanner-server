743d19e519cfb632daf7e4db85f631afeb58291c
Juwon Park <devprofessionalism@gmail.com>
2022-09-13 11:37:34 +0900

    [GOF-166] :wrench: DomainProperties 추가

--- src/main/java/com/forrestgof/jobscanner/common/config/PropertiesConfig.java
+++ src/main/java/com/forrestgof/jobscanner/common/config/PropertiesConfig.java
@@ -5,8 +5,9 @@ import org.springframework.context.annotation.Configuration;
 
 import com.forrestgof.jobscanner.common.config.properties.ApiProperties;
 import com.forrestgof.jobscanner.common.config.properties.AuthProperties;
+import com.forrestgof.jobscanner.common.config.properties.DomainProperties;
 
 @Configuration
-@EnableConfigurationProperties(value = {ApiProperties.class, AuthProperties.class})
+@EnableConfigurationProperties(value = {ApiProperties.class, AuthProperties.class, DomainProperties.class})
 public class PropertiesConfig {
 }
