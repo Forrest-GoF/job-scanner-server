9e5983bfbbecf69b92bd8cda055a9f173fd7d409
Juwon Park <devprofessionalism@gmail.com>
2022-09-13 15:52:14 +0900

    [GOF-166] :adhesive_bandage: signin시 MemberResponse 반환 안하게 수정

--- src/main/java/com/forrestgof/jobscanner/auth/dto/AuthTokenResponse.java
+++ src/main/java/com/forrestgof/jobscanner/auth/dto/AuthTokenResponse.java
@@ -1,7 +1,5 @@
 package com.forrestgof.jobscanner.auth.dto;
 
-import com.forrestgof.jobscanner.member.dto.MemberResponse;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,15 +8,13 @@ import lombok.Data;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class AuthTokenResponse {
 
-	private MemberResponse memberResponse;
 	private String appToken;
 	private String refreshToken;
 
 	public static AuthTokenResponse of(
-		MemberResponse memberResponse,
 		String appToken,
 		String refreshToken
 	) {
-		return new AuthTokenResponse(memberResponse, appToken, refreshToken);
+		return new AuthTokenResponse(appToken, refreshToken);
 	}
 }
