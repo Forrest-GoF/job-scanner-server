7e9f8be6320f9671c15182916c299fdf8da5cf18
Juwon Park <devprofessionalism@gmail.com>
2022-09-13 11:39:38 +0900

    [GOF-166] :sparkles: MailService 인증 메일 전송 기능 개발

--- src/main/java/com/forrestgof/jobscanner/mail/service/DefaultMailService.java
+++ src/main/java/com/forrestgof/jobscanner/mail/service/DefaultMailService.java
@@ -5,8 +5,12 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import com.forrestgof.jobscanner.common.config.properties.DomainProperties;
 import com.forrestgof.jobscanner.common.exception.UndefinedException;
 import com.forrestgof.jobscanner.mail.dto.MailDto;
 
@@ -19,7 +23,10 @@ import lombok.extern.slf4j.Slf4j;
 public class DefaultMailService implements MailService {
 
 	private final JavaMailSender emailSender;
+	private final TemplateEngine templateEngine;
+	private final DomainProperties domainProperties;
 
+	@Async
 	public void send(MailDto mailDto) {
 		MimeMessage message = emailSender.createMimeMessage();
 
@@ -28,7 +35,7 @@ public class DefaultMailService implements MailService {
 
 			helper.setFrom("contact.jobscanner@gmail.com");
 			helper.setSubject(mailDto.getTitle());
-			helper.setText(mailDto.getContent(), false);
+			helper.setText(mailDto.getContent(), true);
 			helper.setTo(mailDto.getAddress());
 		} catch (MessagingException e) {
 			throw new UndefinedException("Mail Error");
@@ -36,5 +43,19 @@ public class DefaultMailService implements MailService {
 
 		emailSender.send(message);
 		log.info("mail send complete.");
+	}
+
+	public void sendAuthenticationMail(String email, String newAppToken) {
+		Context context = new Context();
+		context.setVariable("link",
+			domainProperties.apiServer() + "/auth/mail/authenticate/" + email + "/" + newAppToken);
+		String authenticationMailTemplate = templateEngine.process("authenticationMailTemplate", context);
+
+		send(MailDto.builder()
+			.title("[JobScanner]가입을 환영합니다.")
+			.address(email)
+			.content(authenticationMailTemplate)
+			.build()
+		);
 	}
 }
