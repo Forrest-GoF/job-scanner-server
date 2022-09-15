6ede14e500d5ef6ba45f1074d6510ca0ad7768f8
kang <rilac1@naver.com>
2022-09-15 22:59:41 +0900

    [GOF-201] :fire: 희망 기업 삭제

--- src/main/java/com/forrestgof/jobscanner/member/repository/ObjectiveCompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.forrestgof.jobscanner.member.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.forrestgof.jobscanner.member.domain.ObjectiveCompany;
-
-public interface ObjectiveCompanyRepository extends JpaRepository<ObjectiveCompany, Long> {
-}
