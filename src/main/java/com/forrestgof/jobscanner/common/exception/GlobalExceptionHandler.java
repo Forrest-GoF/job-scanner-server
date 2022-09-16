a4b0e3dfda5865f74b2484501532b94727401eff
Juwon Park <devprofessionalism@gmail.com>
2022-09-16 15:10:49 +0900

    [GOF-195] :recycle: 예외 핸들링에 printStackTrace()추가

--- src/main/java/com/forrestgof/jobscanner/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/forrestgof/jobscanner/common/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.forrestgof.jobscanner.auth.exception.AuthException;
 import com.forrestgof.jobscanner.common.util.CustomResponse;
 
 import lombok.extern.log4j.Log4j2;
@@ -12,21 +13,31 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(AuthException.class)
+	public ResponseEntity authExceptionHandler(AuthException e) {
+		log.error(e);
+		e.printStackTrace();
+		return CustomResponse.error("AuthException: " + e.getMessage());
+	}
+
 	@ExceptionHandler(CustomException.class)
 	public ResponseEntity customExceptionHandle(CustomException e) {
 		log.error(e);
+		e.printStackTrace();
 		return CustomResponse.error(e.getErrorCode());
 	}
 
 	@ExceptionHandler(UndefinedException.class)
 	public ResponseEntity undefinedExceptionHandle(UndefinedException e) {
 		log.error(e);
+		e.printStackTrace();
 		return CustomResponse.error(e.getMessage());
 	}
 
 	@ExceptionHandler
 	public ResponseEntity unexpectedExceptionHandle(Exception e) {
 		log.error(e);
+		e.printStackTrace();
 		return CustomResponse.error(ErrorCode.UNEXPECTED_EXCEPTION);
 	}
 }
