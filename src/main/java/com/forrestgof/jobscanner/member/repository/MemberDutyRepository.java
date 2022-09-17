2f38bfb2e1971380bf0f9fd6c672b8b55b02f84f
kang <rilac1@naver.com>
2022-09-18 05:11:33 +0900

    [GOF-192] :truck: `MemberDuty` 도메인 패키지 이동

--- src/main/java/com/forrestgof/jobscanner/member/repository/MemberDutyRepository.java
+++ src/main/java/com/forrestgof/jobscanner/member/repository/MemberDutyRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.forrestgof.jobscanner.duty.domain.MemberDuty;
+import com.forrestgof.jobscanner.member.domain.MemberDuty;
 import com.forrestgof.jobscanner.member.domain.Member;
 
 public interface MemberDutyRepository extends JpaRepository<MemberDuty, Long> {
