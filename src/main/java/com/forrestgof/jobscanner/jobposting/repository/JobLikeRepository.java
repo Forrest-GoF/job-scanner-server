00e77e6567b6b87a56e7912b70ad9d5ea2ac65c4
kang <rilac1@naver.com>
2022-09-18 08:14:40 +0900

    [GOF-192] :sparkles: 공고 즐겨찾기 기능 구현

--- src/main/java/com/forrestgof/jobscanner/jobposting/repository/JobLikeRepository.java
+++ src/main/java/com/forrestgof/jobscanner/jobposting/repository/JobLikeRepository.java
@@ -1,8 +1,17 @@
 package com.forrestgof.jobscanner.jobposting.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.forrestgof.jobscanner.jobposting.domain.JobLike;
+import com.forrestgof.jobscanner.jobposting.domain.JobPosting;
+import com.forrestgof.jobscanner.member.domain.Member;
 
-public interface JobLikeRepository extends JpaRepository<JobLike, JobLike.JobLikeId> {
+public interface JobLikeRepository extends JpaRepository<JobLike, Long> {
+
+	List<JobLike> findByMember(Member member);
+
+	Optional<JobLike> findByJobPostingAndMember(JobPosting jobPosting, Member member);
 }
