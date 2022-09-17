29a499fdf825304bf38f3054cfb3c6b286c972d2
kang <rilac1@naver.com>
2022-09-18 08:14:10 +0900

    [GOF-192] :sparkles: 공고 즐겨찾기 도메인 수정 + 팩토리 메서드 작성

--- src/main/java/com/forrestgof/jobscanner/jobposting/domain/JobLike.java
+++ src/main/java/com/forrestgof/jobscanner/jobposting/domain/JobLike.java
@@ -1,15 +1,10 @@
 package com.forrestgof.jobscanner.jobposting.domain;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.Embedded;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
@@ -17,29 +12,39 @@ import com.forrestgof.jobscanner.common.entity.BaseTimeEntity;
 import com.forrestgof.jobscanner.member.domain.Member;
 
 import lombok.AccessLevel;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@IdClass(JobLike.JobLikeId.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class JobLike extends BaseTimeEntity {
 
 	@Id
+	@GeneratedValue
+	@Column(name = "job_like_id")
+	private Long id;
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "job_id")
 	private JobPosting jobPosting;
 
-	@Id
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
 
-	@Data
-	public static class JobLikeId implements Serializable {
-		private JobPosting jobPosting;
-		private Member member;
+	private boolean activated;
+
+	public static JobLike of(JobPosting jobPosting, Member member) {
+		JobLike jobLike = new JobLike();
+
+		jobLike.jobPosting = jobPosting;
+		jobLike.member = member;
+
+		return jobLike;
+	}
+
+	public void update(boolean activated) {
+		this.activated = activated;
 	}
 }
