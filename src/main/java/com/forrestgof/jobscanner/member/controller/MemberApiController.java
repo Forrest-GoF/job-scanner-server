7f54fdc213325c56e7911f109eb03c71eb70a1f9
kang <rilac1@naver.com>
2022-09-17 23:26:17 +0900

    [GOF-189] :sparkles: `Member` Patch API 작성

--- src/main/java/com/forrestgof/jobscanner/member/controller/MemberApiController.java
+++ src/main/java/com/forrestgof/jobscanner/member/controller/MemberApiController.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,8 @@ import com.forrestgof.jobscanner.auth.service.AuthService;
 import com.forrestgof.jobscanner.common.dto.CustomResponse;
 import com.forrestgof.jobscanner.member.controller.dto.MemberResponse;
 import com.forrestgof.jobscanner.member.domain.Member;
+import com.forrestgof.jobscanner.member.controller.dto.MemberPatchRequest;
+import com.forrestgof.jobscanner.member.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 public class MemberApiController {
 
 	private final AuthService authService;
+	private final MemberService memberService;
 
 	@GetMapping("")
 	public ResponseEntity<CustomResponse> getMember(HttpServletRequest request) {
@@ -30,5 +35,18 @@ public class MemberApiController {
 		MemberResponse memberResponse = MemberResponse.from(member);
 
 		return CustomResponse.success(memberResponse);
+	}
+
+	@PatchMapping("")
+	public ResponseEntity<CustomResponse> patchMember(
+		HttpServletRequest request,
+		@RequestBody MemberPatchRequest memberUpdateRequest
+	) {
+		String appToken = JwtHeaderUtil.getAccessToken(request);
+
+		Member member = authService.getMemberFromAppToken(appToken);
+		memberService.updateMember(member.getId(), memberUpdateRequest);
+
+		return CustomResponse.success();
 	}
 }
---
+++ src/main/java/com/forrestgof/jobscanner/member/controller/dto/MemberPatchRequest.java
@@ -0,0 +1,13 @@
+package com.forrestgof.jobscanner.member.controller.dto;
+
+import java.util.List;
+import java.util.Optional;
+
+public record MemberPatchRequest(
+	Optional<String> nickname,
+	Optional<String> imageUrl,
+	Optional<List<String>> tags,
+	Optional<List<String>> duties
+) {
+
+}
