4af9952a9b837f799d003f5121a9b21fb32317e7
kang <rilac1@naver.com>
2022-09-17 23:21:26 +0900

    [GOF-189] :sparkles: DutyRepository 조회 메서드 추가

--- src/main/java/com/forrestgof/jobscanner/duty/repository/DutyRepository.java
+++ src/main/java/com/forrestgof/jobscanner/duty/repository/DutyRepository.java
@@ -1,8 +1,12 @@
 package com.forrestgof.jobscanner.duty.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.forrestgof.jobscanner.duty.domain.Duty;
 
 public interface DutyRepository extends JpaRepository<Duty, Long> {
+
+	Optional<Duty> findByName(String name);
 }
