ed80b76341e5afd42a9ce35c385f23c3eaa36d52
Juwon Park <devprofessionalism@gmail.com>
2022-09-20 00:53:14 +0900

    [GOF-196] :adhesive_bandage: GlobalExceptionHandler에 @ResponseStatus 추가

--- src/main/java/com/forrestgof/jobscanner/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/forrestgof/jobscanner/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.forrestgof.jobscanner.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.forrestgof.jobscanner.common.dto.CustomResponse;
@@ -12,12 +14,14 @@ import lombok.extern.log4j.Log4j2;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public CustomResponse<?> handleUnExpectedException(Exception e) {
 		log.error("UnExpected Exception", e);
 		return CustomResponse.error(e);
 	}
 
 	@ExceptionHandler
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public CustomResponse<?> handleCustomException(CustomException e) {
 		log.error("Custom Exception", e);
 		return CustomResponse.customError(e);
