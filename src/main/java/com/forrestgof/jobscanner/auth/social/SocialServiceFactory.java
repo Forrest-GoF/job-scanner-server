8adf7f139e8bef32dd92ccee35186c5f6fe572c3
Juwon Park <devprofessionalism@gmail.com>
2022-09-20 00:34:12 +0900

    [GOF-196] :ambulance: github 소셜로그인 중지

--- src/main/java/com/forrestgof/jobscanner/auth/social/SocialServiceFactory.java
+++ src/main/java/com/forrestgof/jobscanner/auth/social/SocialServiceFactory.java
@@ -14,13 +14,13 @@ public class SocialServiceFactory {
 
 	private final KakaoService kakaoTokenValidator;
 	private final GoogleService googleTokenValidator;
-	private final GithubService githubTokenValidator;
+	// private final GithubService githubTokenValidator;
 
 	public SocialService find(SocialType socialType) {
 		return switch (socialType) {
 			case KAKAO -> kakaoTokenValidator;
 			case GOOGLE -> googleTokenValidator;
-			case GITHUB -> githubTokenValidator;
+			// case GITHUB -> githubTokenValidator;
 		};
 	}
 }
