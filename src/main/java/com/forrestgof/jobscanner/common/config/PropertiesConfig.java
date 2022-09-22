56ed7048cddb65e5db445f9f242eac1f94af007b
Juwon Park <devprofessionalism@gmail.com>
2022-09-22 21:42:09 +0900

    [GOF-214] :arrow_up: awslog 버전 높임(AWS Config 삭제)

--- src/main/java/com/forrestgof/jobscanner/common/config/PropertiesConfig.java
+++ src/main/java/com/forrestgof/jobscanner/common/config/PropertiesConfig.java
@@ -5,15 +5,13 @@ import org.springframework.context.annotation.Configuration;
 
 import com.forrestgof.jobscanner.common.config.properties.ApiProperties;
 import com.forrestgof.jobscanner.common.config.properties.AuthProperties;
-import com.forrestgof.jobscanner.common.config.properties.AwsProperties;
 import com.forrestgof.jobscanner.common.config.properties.DomainProperties;
 
 @Configuration
 @EnableConfigurationProperties(value = {
 	ApiProperties.class,
 	AuthProperties.class,
-	DomainProperties.class,
-	AwsProperties.class
+	DomainProperties.class
 })
 public class PropertiesConfig {
 }
