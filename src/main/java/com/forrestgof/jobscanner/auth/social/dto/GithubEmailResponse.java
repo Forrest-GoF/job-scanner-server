7aa52463d89dac048014d1d333354a640df40510
Juwon Park <devprofessionalism@gmail.com>
2022-09-25 00:37:01 +0900

    [GOF-209] :sparkles: GithubEmailResponse 작성

---
+++ src/main/java/com/forrestgof/jobscanner/auth/social/dto/GithubEmailResponse.java
@@ -0,0 +1,15 @@
+package com.forrestgof.jobscanner.auth.social.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GithubEmailResponse {
+
+	private String email;
+}
