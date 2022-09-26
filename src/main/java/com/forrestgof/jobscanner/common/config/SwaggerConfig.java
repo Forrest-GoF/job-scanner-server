f6fd2cb7590fdbb7c5c557da23fa92c2ae892230
Juwon Park <devprofessionalism@gmail.com>
2022-09-26 22:10:06 +0900

    [GOF-214] :wrench: actuator 사용을 위해 SwaggerConfig 업데이트

--- src/main/java/com/forrestgof/jobscanner/common/config/SwaggerConfig.java
+++ src/main/java/com/forrestgof/jobscanner/common/config/SwaggerConfig.java
@@ -1,7 +1,14 @@
 package com.forrestgof.jobscanner.common.config;
 
+import java.lang.reflect.Field;
+import java.util.List;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -9,6 +16,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
 
 @Configuration
 public class SwaggerConfig {
@@ -30,5 +39,42 @@ public class SwaggerConfig {
 			// .description("springboot rest api practice.")
 			.version("1.0")
 			.build();
+	}
+
+	@Bean
+	public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+		return new BeanPostProcessor() {
+
+			@Override
+			public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+				if (bean instanceof WebMvcRequestHandlerProvider ||
+					bean instanceof WebFluxRequestHandlerProvider
+				) {
+					customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+				}
+				return bean;
+			}
+
+			private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings
+			) {
+				List<T> copy = mappings.stream()
+					.filter(mapping -> mapping.getPatternParser() == null)
+					.toList();
+				mappings.clear();
+				mappings.addAll(copy);
+			}
+
+			@SuppressWarnings("unchecked")
+			private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+				try {
+					Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+					assert field != null;
+					field.setAccessible(true);
+					return (List<RequestMappingInfoHandlerMapping>)field.get(bean);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					throw new IllegalStateException(e);
+				}
+			}
+		};
 	}
 }
