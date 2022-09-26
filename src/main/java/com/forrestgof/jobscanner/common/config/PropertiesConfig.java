a9ddd4194262a7627b4b9b5ce6776f6a1201e8aa
Juwon Park <devprofessionalism@gmail.com>
2022-09-26 16:39:42 +0900

    [GOF-213] :wrench: AwsProperties 생성

--- src/main/java/com/forrestgof/jobscanner/common/config/PropertiesConfig.java
+++ src/main/java/com/forrestgof/jobscanner/common/config/PropertiesConfig.java
@@ -5,13 +5,15 @@ import org.springframework.context.annotation.Configuration;
 
 import com.forrestgof.jobscanner.common.config.properties.ApiProperties;
 import com.forrestgof.jobscanner.common.config.properties.AuthProperties;
+import com.forrestgof.jobscanner.common.config.properties.AwsProperties;
 import com.forrestgof.jobscanner.common.config.properties.DomainProperties;
 
 @Configuration
 @EnableConfigurationProperties(value = {
 	ApiProperties.class,
 	AuthProperties.class,
-	DomainProperties.class
+	DomainProperties.class,
+	AwsProperties.class
 })
 public class PropertiesConfig {
 }
